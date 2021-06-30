84b781b7fa9a348c3726e0a406c7c2585074ba63
Timelas <81299321+Timelas@users.noreply.github.com>
2021-06-30 18:48:38 +0300

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,55 @@
+const validationConfig = {
+  formSelector: '.form',
+  inputSelector: '.form__input',
+  submitButtonSelector: '.form__button',
+  inactiveButtonClass: 'form__button_disabled'
+};
+
+const hasInvalidInput = (inputs) => {
+  return Array.from(inputs).some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+};
+
+const toggleButton = (button, validationConfig, inputs) => {
+  if (hasInvalidInput(inputs)) {
+    button.classList.add(validationConfig.inactiveButtonClass);
+    button.disabled = true;
+  }
+  else {
+    button.classList.remove(validationConfig.inactiveButtonClass);
+    button.disabled = false;
+  }
+};
+
+const inputListeners = (formElement, validationConfig) => {
+  const inputs = Array.from(formElement.querySelectorAll(validationConfig.inputSelector));
+  const button = formElement.querySelector(validationConfig.submitButtonSelector);
+  toggleButton(button, validationConfig, inputs);
+  inputs.forEach(inputElement => {
+    inputElement.addEventListener('input', () => {
+      toggleButton(button, validationConfig, inputs);
+    });
+  });
+};
+
+const enableValidation = (validationConfig) => {
+  const forms = document.querySelectorAll(validationConfig.formSelector);
+  forms.forEach(formElement => {
+    formElement.addEventListener('submit', preventFormSubmit);
+    inputListeners(formElement, validationConfig);
+    });
+};
+
+function preventFormSubmit(evt) {
+  evt.preventDefault();
+};
+
+
+const disableSubmitButton = () => {
+  const button = document.querySelector(validationConfig.submitButtonSelector);
+  button.setAttribute('disabled', 'true');
+  button.classList.add('popup__button_disabled');
+};
+
+enableValidation(validationConfig);
