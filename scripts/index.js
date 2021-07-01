03b5d26e02a850706767a26f9b65ed8efd0040fa
Timelas <81299321+Timelas@users.noreply.github.com>
2021-07-01 10:32:33 +0300

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,57 +1,79 @@
-const validationConfig = {
-  formSelector: '.form__container',
-  inputSelector: '.form__input',
-  submitButtonSelector: '.form__button',
-  inactiveButtonClass: 'form__button_disabled'
+const formValid = {
+  nameValidity: true,
+  alcoholValidity : false,
+  hotDishesValidity: false,
+  garnirValidity: false,
+  presenceValidity: false
+}
+
+const form = document.forms.form;
+const formInput = form.elements.name;
+const alcohol = form.elements.alcohol;
+const hotDishes = form.elements.hot_dish;
+const garnir = form.elements.garnish;
+const presence = form.elements.presence;
+const button = document.querySelector('.form__button');
+
+function isFormValid() {
+  const formValidArray = Object.values(formValid);
+  const result = formValidArray.every(e => e);
+  if (result) {
+    button.classList.remove('form__button_disabled');
+    button.removeAttribute('disabled');
+  } else {
+    button.classList.add('form__button_disabled');
+    button.setAttribute('disabled', 'true');
+  };
 };
 
-const hasInvalidInput = (inputs) => {
-  return Array.from(inputs).some((inputElement) => {
-    return !inputElement.validity.valid;
+formInput.addEventListener('input', (item) => {
+  formValid.nameValidity = item.target.validity.valid;
+  isFormValid();
+});
+
+
+alcohol.forEach(item => {
+  item.addEventListener('change', () => {
+    alcoholValidity()
   });
-};
+});
 
-const toggleButton = (button, validationConfig, inputs) => {
-  if (hasInvalidInput(inputs)) {
-    button.classList.add(validationConfig.inactiveButtonClass);
-    button.disabled = true;
-  }
-  else {
-    button.classList.remove(validationConfig.inactiveButtonClass);
-    button.disabled = false;
-  }
-};
-
-const inputListeners = (formElement, validationConfig) => {
-  const inputs = Array.from(formElement.querySelectorAll(validationConfig.inputSelector));
-  const button = formElement.querySelector(validationConfig.submitButtonSelector);
-  toggleButton(button, validationConfig, inputs);
-  inputs.forEach(inputElement => {
-    inputElement.addEventListener('input', () => {
-      toggleButton(button, validationConfig, inputs);
-    });
+hotDishes.forEach(item => {
+  item.addEventListener('change', () => {
+    isRadioChecked(hotDishes, 'hotDishesValidity');
   });
+});
+
+garnir.forEach(item => {
+  item.addEventListener('change', () => {
+    isRadioChecked(garnir, 'garnirValidity');
+  });
+});
+
+presence.forEach(item => {
+  item.addEventListener('change', () => {
+    isRadioChecked(presence, 'presenceValidity');
+  });
+});
+
+ function alcoholValidity() {
+  formValid.alcoholValidity = Array.from(alcohol).some(item => item.checked);
+  isFormValid();
 };
 
-const enableValidation = (validationConfig) => {
-  const forms = document.querySelectorAll(validationConfig.formSelector);
-  forms.forEach(formElement => {
-    formElement.addEventListener('submit', preventFormSubmit);
-    inputListeners(formElement, validationConfig);
-    });
+function isRadioChecked(radios, name) {
+  formValid[name] = Array.from(radios).some(item => item.checked);
+  isFormValid();
 };
 
-function preventFormSubmit(evt) {
+isFormValid();
+
+button.addEventListener('submit', (evt) => {
   evt.preventDefault();
-};
+  form.reset();
+});
 
 
-const disableSubmitButton = () => {
-  const button = document.querySelector(validationConfig.submitButtonSelector);
-  button.setAttribute('disabled', 'true');
-  button.classList.add('popup__button_disabled');
-};
 
-enableValidation(validationConfig);
 
 
